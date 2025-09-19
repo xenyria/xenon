b1e90f3c7bf97ecfc2681cc0bb358d35a043c8e2
Luma <me@luma.fm>
2025-09-19 14:08:08 +0200

    refactor: Use IVec3D instead of Vec3D for camera position

--- Core/src/main/kotlin/net/xenyria/xenon/server/XenonClient.kt
+++ Core/src/main/kotlin/net/xenyria/xenon/server/XenonClient.kt
@@ -1,12 +1,12 @@
 package net.xenyria.xenon.server
 
-import net.xenyria.xenon.core.Vec3D
+import net.xenyria.xenon.core.IVec3D
 import net.xenyria.xenon.packet.IXenonPacket
 import net.xenyria.xenon.packet.sendPacket
 
 interface IXenonClient {
 
-    val cameraPosition: Vec3D
+    val cameraPosition: IVec3D
 
     fun sendPluginMessage(channel: String, data: ByteArray)
 
